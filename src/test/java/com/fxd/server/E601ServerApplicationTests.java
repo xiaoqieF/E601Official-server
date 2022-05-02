649c264a5b9e7020e31a80ff1fec181070966158
xiaodongFan <fanxiaodong@buaa.edu.cn>
2022-05-02 16:15:50 +0800

    初始化项目

---
+++ src/test/java/com/fxd/server/E601ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxd.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E601ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
